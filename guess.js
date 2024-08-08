e12ee64fc4ce0fea0c8f0e86fcf594bbbfd84812
EvelynLR <evelynguan@yahoo.com>
2024-08-08 15:42:20 -0600

    Update guess.js

--- guess.js
+++ guess.js
@@ -2,14 +2,14 @@
 const prompt = require("syncprompt");
 const randomNumber = Math.ceil(Math.random() * 20);
 let numGuesses=1;
-let guess = prompt("guess a number between 1 - 20: ");
+let guess = 0;
 while (guess!=randomNumber){
-guess = prompt("guess a number between 1 - 20: ");
+guess = Number(prompt("guess a number between 1 - 20: "));
 if(guess==randomNumber){
     console.log("You won! You took ", numGuesses, "guesses.");
-} else if (guess>randomNumber) {
+} else if (guess > randomNumber) {
     console.log("You guessed too high.");
-} else if (guess<randomNumber){
+} else if (guess < randomNumber){
     console.log("You guess too low.")
 }
 numGuesses++;
