461653dca2bce9ad86d1b83b61058fe4f3e7af38
EvelynLR <evelynguan@yahoo.com>
2024-08-08 15:11:13 -0600

    first time commit

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 // Do not remove this line
 const prompt = require("syncprompt");
+const randomNumber = Math.ceil(Math.random() * 20);
+let timer=0;
+do {
+let guess = prompt("guess a number between 1 - 20: ");
+if(guess==randomNumber){
+    console.log("You won!");
+} else if (guess>randomNumber) {
+    console.log("You guessed too high.");
+} else{
+    console.log("You guess too low.")
+}
+timer++;
+} while (guess!=randomNumber);
